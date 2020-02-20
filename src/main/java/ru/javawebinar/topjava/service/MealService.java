2f77e52b00ae7c95e06fbd23439c23085c0d61f4
AlekPo <al0do@yandex.ru>
2020-02-20 17:17:32 +0300

    HW02 -Spring initialization in MealServlet

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -7,10 +8,12 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import java.time.LocalDate;
 import java.util.Collection;
 
+import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
+    private static final Logger log = getLogger(MealService.class);
 
     private final MealRepository repository;
 
@@ -19,26 +22,32 @@ public class MealService {
     }
 
     public Meal save(Meal meal, int userId) {
+        log.info("save {}", meal);
         return repository.save(meal, userId);
     }
 
     public void update(Meal meal, int userId) {
+        log.info("update {} with id={}", meal, meal.getId());
         checkNotFoundWithId(repository.save(meal, userId), meal.getId());
     }
 
     public void delete(int id, int userId) {
+        log.info("delete {}", id);
         checkNotFoundWithId(repository.delete(id, userId), id);
     }
 
     public Meal get(int id, int userId) {
+        log.info("get {}", id);
         return checkNotFoundWithId(repository.get(id, userId), id);
     }
 
     public Collection<Meal> getAll(int userId) {
+        log.info("getAll");
         return repository.getAll(userId);
     }
 
     public Collection<Meal> getAllForDate(int userId, LocalDate startDate, LocalDate endDate) {
+        log.info("getAllForDateTime");
         return repository.getAllForDate(userId, startDate, endDate);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
@@ -24,7 +23,7 @@ public class MealRestController {
 
     private MealService service;
 
-    @Autowired
+    //    @Autowired
     public MealRestController(MealService service) {
         this.service = service;
     }
