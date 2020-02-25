602eba3f7e537051be50073e3db531c6b30aa113
AlekPo <al0do@yandex.ru>
2020-02-25 16:45:21 +0300

    HW03 -JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +11,9 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+('20150601 13:00:00', 'Пользователь ланч', '500', 100000),
+('20150601 20:00:00', 'Пользователь ужин', '1400', 100000),
+('20150601 14:00:00', 'Админ ланч', '510', 100001),
+('20150601 21:00:00', 'Админ ужин', '1500', 100001);
