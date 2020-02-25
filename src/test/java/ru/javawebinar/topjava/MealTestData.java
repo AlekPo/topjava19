0a6e09050f3bd7d3838586c1cd2065c7be6dc39b
AlekPo <al0do@yandex.ru>
2020-02-25 21:11:15 +0300

    HW03 -Optional -fix1 MealServiceTest

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -7,10 +7,10 @@ import java.time.Month;
 
 public class MealTestData {
 
-    public static final Meal MEAL_1 = new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 13, 0), "Пользователь ланч", 500);
-    public static final Meal MEAL_2 = new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 20, 0), "Пользователь ужин", 1400);
-    public static final Meal MEAL_3 = new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510);
-    public static final Meal MEAL_4 = new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500);
+    public static final Meal MEAL_1 = new Meal(101, LocalDateTime.of(2015, Month.JUNE, 1, 13, 0), "Пользователь ланч", 500);
+    public static final Meal MEAL_2 = new Meal(102, LocalDateTime.of(2015, Month.JUNE, 1, 20, 0), "Пользователь ужин", 1400);
+    public static final Meal MEAL_3 = new Meal(103, LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510);
+    public static final Meal MEAL_4 = new Meal(104, LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500);
 
     public static Meal getNew() {
         return new Meal(LocalDateTime.now(), "Новая еда", 500);
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -11,9 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static ru.javawebinar.topjava.MealTestData.MEAL_1;
-import static ru.javawebinar.topjava.MealTestData.getNew;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
 
 @ContextConfiguration({
@@ -33,38 +36,57 @@ public class MealServiceTest {
     @Autowired
     private MealService service;
 
-
     @Test
-    public void get() throws Exception {
-        Meal meal = service.get(MEAL_1.getId(), USER.getId());
-        assertThat(meal).isEqualTo(MEAL_1);
-    }
-
-    @Test(expected = NotFoundException.class)
-    public void delete() throws Exception {
-        service.delete(MEAL_1.getId(), USER.getId());
-        service.get(MEAL_1.getId(), USER.getId());
-    }
-
-    @Test
-    public void getBetweenHalfOpen() {
-    }
-
-    @Test
-    public void getAll() {
-    }
-
-    @Test
-    public void update() {
-    }
-
-    @Test
-    public void create() throws Exception {
+    public void create() {
         Meal newMeal = getNew();
         Meal created = service.create(newMeal, USER.getId());
         Integer newId = created.getId();
         newMeal.setId(newId);
         assertThat(created).isEqualTo(newMeal);
         assertThat(service.get(newId, USER.getId())).isEqualTo(newMeal);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void delete() {
+        service.delete(MEAL_1.getId(), USER.getId());
+        service.get(MEAL_1.getId(), USER.getId());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deletedNotFound() {
+        service.delete(1, 1);
+    }
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL_1.getId(), USER.getId());
+        assertThat(meal).isEqualTo(MEAL_1);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() {
+        service.get(1, 1);
+    }
+
+    @Test
+    public void getBetweenHalfOpen() {
+        List<Meal> meals = service.getBetweenHalfOpen(null, null, USER.getId());
+        assertThat(meals).isEqualTo(Arrays.asList(MEAL_2, MEAL_1));
+    }
+
+    @Test
+    public void getAll() {
+        List<Meal> all = service.getAll(USER.getId());
+        assertThat(all).isEqualTo(Arrays.asList(MEAL_2, MEAL_1));
+    }
+
+    @Test
+    public void update() {
+        Meal updated = new Meal(MEAL_1);
+        updated.setDateTime(LocalDateTime.now());
+        updated.setDescription("UpdatedMeal");
+        updated.setCalories(111);
+        service.update(updated, USER.getId());
+        assertThat(service.get(updated.getId(), USER.getId())).isEqualTo(updated);
     }
 }
